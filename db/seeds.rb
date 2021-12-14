716161027036195aec24110f415113b4b4465b5e
Pierce <piercedavidraphael@gmail.com>
2021-12-14 09:21:14 -0600

    Completed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,4 @@ Cheese.create!(name: 'Cheddar', price: 3, is_best_seller: true)
 Cheese.create!(name: 'Pepper Jack', price: 4, is_best_seller: true)
 Cheese.create!(name: 'Limburger', price: 8, is_best_seller: false)
 
-puts '✅ Done seeding'
+puts '✅🧀 Done cheesing'
